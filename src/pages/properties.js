8de3cd2b0938fb30c9f0b7a3a18649bf308b83d4
ayabbear <jonnah.frances@gmai.com>
2023-04-14 12:54:18 +0800

    remove crlf

--- src/pages/properties.js
+++ src/pages/properties.js
@@ -1,4 +1,5 @@
 import Paragraph from "@components/paragraph";
+import Title from "@components/title";
 
 export default function Properties() {
   return (
@@ -7,6 +8,7 @@ export default function Properties() {
         <p>PROPERTIES</p>
       </div>
       <div>
+        <Title name="A" school="B" mall="C" />
         <Paragraph name="A" school="B" mall="C" />
       </div>
     </>
