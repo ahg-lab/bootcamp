593e0351e8d25afca28a4347c7f6e4b61be145e1
ayabbear <jonnah.frances@gmai.com>
2023-04-25 17:18:13 +0800

    embed

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,3 +1,4 @@
+import FooterItem from '@components/footer-item'
 import Title from '@components/title'
 import { useState } from 'react'
 
@@ -36,6 +37,34 @@ const HomepageContainer = () => {
       <div>
         <Title name="A" school="B" mall="C" />
       </div>
+      {/* sample */}
+      {/* 1 rem = 16px */}
+      <div className="flex flex-col w-full p-1 space-y-4 lg:space-y-0 lg:space-x-4 lg:flex-row">
+        <div className="w-4/12 p-20 bg-sky-400">box 1</div>
+        <div className="w-5/12 p-20 bg-pink-400">box 2</div>
+        <div className="w-3/12 p-20 bg-rose-500">box 3</div>
+      </div>
+      {/* kapag lampas 1024 naka row */}
+      <div className="flex flex-col w-full p-1 space-y-4 lg:space-y-0 lg:space-x-4 lg:flex-row">
+        <div className="w-full p-20 lg:w-3/12 bg-rose-500">box 3</div>
+        <div className="w-full p-20 lg:w-5/12 bg-sky-400">box 1</div>
+        <div className="w-full p-20 bg-pink-400 lg:w-4/12">box 2</div>
+      </div>
+      <div>
+        <FooterItem
+          title="ONLINE SERVICES"
+          items={['PUP WebMail', 'PUP iApply', 'SIS for Students', 'SIS for Faculty']}
+        />
+      </div>
+      <iframe
+        // style="border-radius:12px"
+        src="https://open.spotify.com/embed/playlist/37i9dQZF1DX6T5dWVv97mp?utm_source=generator&theme=0"
+        width="50%"
+        height="352"
+        allowfullscreen=""
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+      />
     </>
   )
 }
